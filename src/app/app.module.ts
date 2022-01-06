f2062a8c9acf358ad1207e4e5c218fc9b7239f0c
calvinkoch00 <calvin.koch00@yahoo.de>
2022-01-06 02:51:45 +0100

    maintanance design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaintananceComponent } from './maintanance/maintanance.component';
 
 @NgModule({
-  declarations: [				
+  declarations: [					
     AppComponent,
+      MaintananceComponent
    ],
   imports: [
     BrowserModule,
