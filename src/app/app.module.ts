527c9d3887296135fa25737b3995b57a63af810e
Calvin Koch <contact@calvinkoch.ch>
2022-05-14 15:15:39 +0200

    Build Staged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaintananceComponent } from './maintanance/maintanance.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [						
     AppComponent,
       MaintananceComponent,
-      TestComponent
    ],
   imports: [
     BrowserModule,
